c76aaf88ae745a2cc4a369ef0a307492da3ea2c9
Navtoj Chahal <github@navtoj.com>
2024-11-22 16:46:14 -0800

    Add Dark Mode
    `bun add mode-watcher`

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<ModeWatcher themeColors={{ dark: 'black', light: 'white' }} />
 
 {@render children()}
